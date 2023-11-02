8d438d9eed99455693e69ca2264fffca5332b986
ameliakim <131293970+ameliakim@users.noreply.github.com>
2023-11-02 13:38:40 -0400

    added apply function

--- hierachial clustering.R
+++ hierachial clustering.R
@@ -1,4 +1,4 @@
-Cluster_Ex<-read.csv("D:/Dickinson College Teaching/DATA 180/data/Cluster_Ex.csv") #"Path where your data is goes here"
+Cluster_Ex<-read.csv("D:/Dickinson College Teaching/DATA-180-AmeliaKim/datas/Cluster_Ex.csv") #"Path where your data is goes here"
 ls(Cluster_Ex)
 head(Cluster_Ex)
 
@@ -40,3 +40,11 @@ plot(Cluster_Ex_HC,cex=1.25,xlab="")
 
 
 plot(Cluster_Ex,hang=-1,main="Main",sub="Sub",lwd=2,cex=.8,xlab="",frame.plot=T)
+
+install.packages("ggdendro")
+library(ggdendro)
+
+data()
+
+
+
